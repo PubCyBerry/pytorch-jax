3403e8781d4047985554d62a5c015d041dae4e07
PubCyBerry <kth7186@gmail.com>
2023-06-13 13:12:59 +0900

    initial commit

---
+++ src/utils/utils.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+import torch
+import random
+
+def set_seed(seed: float = 41) -> None:
+    """
+    Set the random seed for various libraries to ensure reproducibility.
+
+    Args:
+        seed (float): The seed value to set. Defaults to 41.
+
+    Returns:
+        None
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+def is_notebook():
+    try:
+        from IPython import get_ipython
+
+        if "IPKernelApp" not in get_ipython().config:  # pragma: no cover
+            raise ImportError("console")
+            return False
+        if "VSCODE_PID" in os.environ:  # pragma: no cover
+            raise ImportError("vscode")
+            return False
+    except:
+        return False
+    else:  # pragma: no cover
+        return True
