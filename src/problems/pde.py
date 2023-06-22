6b0126cb17a57dd987dd4148dd909053f9c083a3
taehun.kim <taehun.kim@konantech.com>
2023-06-22 17:27:10 +0900

    add: diy fft diff

--- src/problems/pde.py
+++ src/problems/pde.py
@@ -1,5 +1,24 @@
+import numpy as np
 from numpy.typing import ArrayLike
-from scipy import fftpack
+
+
+def fft_diff(x: ArrayLike, order: int = 1, period: float = 2 * np.pi):
+    """Spectral Differentiation.
+
+    Args:
+        x (jax.Array): 1D sequence to be differentiated.
+        order (int, optional): The order of differentiation. Defaults to 1.
+        period (float, optional): The assumed period of the sequence. Defaults to 2*pi.
+    """
+    Nx: int = len(x)
+    dx: float = period / Nx
+    # wave number
+    k = 2 * np.pi * np.fft.fftfreq(Nx, dx)
+    # Spatial Derivative in the Fourier domain
+    x_hat = np.power(1j * k, order) * np.fft.fft(x)
+    # Switching in the spatial domain
+    x_diff = np.fft.ifft(x_hat)
+    return x_diff.real
 
 
 # Define pseudo-spectral solver
@@ -12,8 +31,8 @@ def burgers(t: float, u: ArrayLike, period: float, nu: float):
     period: assumed period of sequence
     coefficient, nu: kinematic viscosity
     """
-    u_x = fftpack.diff(u, order=1, period=period)  # first derivative
-    u_xx = fftpack.diff(u, order=2, period=period)  # second derivative
+    u_x = fft_diff(u, order=1, period=period)  # first derivative
+    u_xx = fft_diff(u, order=2, period=period)  # second derivative
     u_t = -u * u_x + nu * u_xx
 
     return u_t
@@ -29,7 +48,7 @@ def kdv(t: float, u: ArrayLike, period: float, coefficient: float):
     period: assumed period of sequence
     coefficient: kinematic viscosity
     """
-    u_x = fftpack.diff(u, order=1, period=period)
-    u_xxx = fftpack.diff(u, order=3, period=period)
+    u_x = fft_diff(u, order=1, period=period)
+    u_xxx = fft_diff(u, order=3, period=period)
     u_t = -u * u_x - coefficient * u_xxx
     return u_t
